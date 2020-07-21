5ab8fb8a9a52eacacce5895010c38439ba59e6a1
Cassio Murilo <cmurilo.san@gmail.com>
2020-07-21 15:40:15 -0300

    Aula 02_B - Inserindo com Statement

---
+++ src/TestaInsercao.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaInsercao {
+	
+	public static void main(String[] args) throws SQLException{
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.recuperarConexao();
+		
+		Statement stm = connection.createStatement();
+		stm.execute(
+				"INSERT INTO PRODUTO (nome, descricao) "
+				+ "VALUES ('Mouse', 'Mouse sem fio')",
+				Statement.RETURN_GENERATED_KEYS);
+//		Atenção: aspas simples para mysql e aspas duplas para o java
+		
+		ResultSet rst = stm.getGeneratedKeys();
+		while(rst.next()) {
+			Integer id = rst.getInt(1);
+			System.out.println("O id criado foi: " + id);
+		}
+		
+	}
+
+}
