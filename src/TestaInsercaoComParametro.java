0837f39a1557eea7ac804da039761369a40de8f0
Cassio Murilo <cmurilo.san@gmail.com>
2020-07-23 22:33:36 -0300

    Aula 04_A - Assumindo o Controle

--- src/TestaInsercaoComParametro.java
+++ src/TestaInsercaoComParametro.java
@@ -8,20 +8,27 @@ public class TestaInsercaoComParametro {
 	
 	public static void main(String[] args) throws SQLException {
 		
-		String nome = "Mouse";
-		String descricao = "Mouse sem fio";
-		
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
+		connection.setAutoCommit(false);	//Assume o controle da transação e tira a responsabilidade do JDBC
 		
-//		PreparedStatement deixa o código muito mais seguro, sem ter o risco de uma inserção deletar todo nosso banco, por exemplo
 		PreparedStatement stm = connection.prepareStatement(
 				"INSERT INTO PRODUTO (nome, descricao)"	+ "VALUES (?, ?)", 
 				Statement.RETURN_GENERATED_KEYS);
 		
+		
+		adicionarVariavel("SmartTV", "60 polegadas", stm);
+		adicionarVariavel("Celular", "Iphone 11", stm);
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
-		
+		/*
+		if (nome.equals("Radio")) {
+			throw new RuntimeException("Não foi possível adicionar o produto");
+		}
+		*/
 		stm.execute();
 		
 		ResultSet rst = stm.getGeneratedKeys();
@@ -29,6 +36,7 @@ public class TestaInsercaoComParametro {
 			Integer id = rst.getInt(1);
 			System.out.println("O id criado foi: " + id);
 		}
+		rst.close();
 	}
 
 }
