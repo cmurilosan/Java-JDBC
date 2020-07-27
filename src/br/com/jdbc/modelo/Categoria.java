cb14e031faabfc74e779d0760c2d8a18664205e5
Cassio Murilo <cmurilo.san@gmail.com>
2020-07-27 18:03:20 -0300

    Aula 07_C - Usando Inner Join

--- src/br/com/jdbc/modelo/Categoria.java
+++ src/br/com/jdbc/modelo/Categoria.java
@@ -1,9 +1,13 @@
 package br.com.jdbc.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categoria {
 	
 	private Integer id;
 	private String nome;
+	private List<Produto> produtos = new ArrayList<Produto>();
 	
 	public Categoria(Integer id, String nome) {
 		super();
@@ -17,6 +21,14 @@ public class Categoria {
 
 	public int getId() {
 		return this.id;
+	}
+
+	public void adicionar(Produto produto) {
+		produtos.add(produto);
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
 	}
 	
 	
