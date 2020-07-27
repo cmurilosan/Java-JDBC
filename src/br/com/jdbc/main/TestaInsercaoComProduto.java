3784a9beed9ec0c12eaadb3f6eecfe729b269cb4
Cassio Murilo <cmurilo.san@gmail.com>
2020-07-26 22:51:56 -0300

    Aula 06_B - DAO com inserção do produto

--- src/br/com/jdbc/main/TestaInsercaoComProduto.java
+++ src/br/com/jdbc/main/TestaInsercaoComProduto.java
@@ -1,14 +1,9 @@
 package br.com.jdbc.main;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-
-import com.mysql.cj.protocol.Resultset;
-
 import br.com.jdbc.ConnectionFactory;
+import br.com.jdbc.dao.ProdutoDAO;
 import br.com.jdbc.modelo.Produto;
 
 public class TestaInsercaoComProduto {
@@ -18,27 +13,10 @@ public class TestaInsercaoComProduto {
 		Produto comoda = new Produto("Cômoda", "Cômoda Vertical");
 		
 		try(Connection connection= new ConnectionFactory().recuperarConexao()) {
-			
-			String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)";
-			
-			try(PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-				
-				pstm.setString(1, comoda.geNome());
-				pstm.setString(2, comoda.getDescricao());
-				
-				pstm.execute();
-				
-				try(ResultSet rst = pstm.getGeneratedKeys()) {
-					while(rst.next()) {
-						comoda.setId(rst.getInt(1));
-					}
-				}
-				
-			}
-			
+			ProdutoDAO produtoDao = new ProdutoDAO(connection);
+			produtoDao.salvar(comoda);
+			// Lista = persistenciaProduto.listar();	Caso queira listar
 		}
-		
-		System.out.println(comoda);
 
 	}
 
